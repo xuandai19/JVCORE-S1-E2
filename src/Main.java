bd6a1dfbe9cc40486b73556a7b11830cb80691aa
Xuân Đại <daik5xuanvinh@gmail.com>
2026-04-21 22:10:46 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final float PI = 3.14f;
+
+        System.out.print("bán kính : ");
+        float k1 = sc.nextFloat();
+        System.out.printf("Diện tích : %.2f", PI * k1 * k1);
+    }
 }
